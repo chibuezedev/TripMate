65478c36f99aadedc3fedca06a9cae3d75579717
Paul Chibueze <chibuezedevloper@gmail.com>
2024-06-29 16:30:00 +0100

    update

--- app.js
+++ app.js
@@ -75,10 +75,43 @@ function displayAreaRecommendations(areas) {
 function displayMessage(message, sender) {
   const messageElement = document.createElement("p");
   messageElement.classList.add("chat-message");
-  messageElement.classList.add(sender === "bot" ? "bot-message" : "user-message");
+  messageElement.classList.add(
+    sender === "bot" ? "bot-message" : "user-message"
+  );
   messageElement.innerHTML = message;
   chatMessages.appendChild(messageElement);
 
   // Scroll to bottom of messages
   chatMessages.scrollTop = chatMessages.scrollHeight;
+}
+
+// Home section
+
+let opened = false;
+const mobileBurger = document.querySelector("#mobile-burger");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileTop = document.querySelector("#mobile-burger .mobile-burger .top");
+const mobileBtm = document.querySelector(
+  "#mobile-burger .mobile-burger .bottom"
+);
+const body = document.querySelector("body");
+
+mobileBurger.addEventListener("click", showMenu);
+
+function showMenu(e) {
+  e.preventDefault();
+  opened = !opened;
+
+  if (opened) {
+    mobileTop.className += " opened";
+    mobileBtm.className += " opened";
+    mobileMenu.style.margin = "0";
+    body.style.overflow = "hidden";
+  } else {
+    mobileTop.className = "top";
+    mobileBtm.className = "bottom";
+    mobileMenu.style.margin = "-460px 0 0 0";
+    body.style.overflow = "scroll";
+    body.style.overflowX = "hidden";
+  }
 }
